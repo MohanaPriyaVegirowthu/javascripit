d357d074398c466ec0d2c7b5a82f919a01459f22
MohanaPriyaVegirowthu <mohana.priya@deeplogictech.com>
2022-10-25 12:45:54 +0530

    oct21

--- oct 20/index.js
+++ oct 20/index.js
@@ -13,3 +13,31 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const bcrypt = require('bcryptjs')
+
+const myFunction = async () => {
+    const password = 'Priya@123'
+    const hashedPassword = await bcrypt.hash(password, 8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    const isMatch = await bcrypt.compare('priya@123', hashedPassword)
+    console.log(isMatch)
+}
+
+myFunction()
+
+
+const jwt = require('jsonwebtoken')
+
+const Function = async () => {
+    const token = jwt.sign({ _id: 'dbc123' }, 'thisnodejscourse', { expiresIn: '7 days' })
+    console.log(token)
+
+    const data = jwt.verify(token, 'thisnodejscourse')
+    console.log(data)
+}
+
+Function()
--- oct 20/router/user.js
+++ oct 20/router/user.js
@@ -7,11 +7,13 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })     
     } catch (e) {
         res.status(400).send(e)
     }
 })
+
 
 router.get('/users', async (req, res) => {
     try {
@@ -21,7 +23,15 @@ router.get('/users', async (req, res) => {
         res.status(500).send()
     }
 })
-
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (e) {
+        res.status(400).send()
+    }
+})
 router.get('/users/:id', async (req, res) => {
     const _id = req.params.id
 
@@ -38,6 +48,27 @@ router.get('/users/:id', async (req, res) => {
     }
 })
 
+// router.patch('/users/:id', async (req, res) => {
+//     const updates = Object.keys(req.body)
+//     const allowedUpdates = ['name', 'email', 'password', 'age']
+//     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+//     if (!isValidOperation) {
+//         return res.status(400).send({ error: 'Invalid updates!' })
+//     }
+
+//     try {
+//         const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+
+//         if (!user) {
+//             return res.status(404).send()
+//         }
+
+//         res.send(user)
+//     } catch (e) {
+//         res.status(400).send(e)
+//     }
+// })
 router.patch('/users/:id', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
@@ -48,7 +79,10 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()
 
         if (!user) {
             return res.status(404).send()
@@ -59,6 +93,7 @@ router.patch('/users/:id', async (req, res) => {
         res.status(400).send(e)
     }
 })
+
 
 router.delete('/users/:id', async (req, res) => {
     try {
